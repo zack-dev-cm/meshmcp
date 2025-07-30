e8f642d3ff3d4a5c7992580d2274492d22f33057
zack-dev-cm <112114097+zack-dev-cm@users.noreply.github.com>
2025-07-30 20:30:03 +0400

    clean

--- android_app/app/build/tmp/kapt3/stubs/debug/com/example/bitchat/BluetoothMeshServiceKt.java
+++
@@ -1,9 +0,0 @@
-package com.example.bitchat;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\n\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\"\u0014\u0010\u0000\u001a\u00020\u00018BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u0002\u0010\u0003\u00a8\u0006\u0004"}, d2 = {"appContext", "Landroid/content/Context;", "getAppContext", "()Landroid/content/Context;", "app_debug"})
-public final class BluetoothMeshServiceKt {
-    
-    private static final android.content.Context getAppContext() {
-        return null;
-    }
-}
