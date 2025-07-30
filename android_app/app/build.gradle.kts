2a0940fb2eb7a964ee9d8aff872bf9b0f132ab78
zack-dev-cm <superkerascv@gmail.com>
2025-07-30 17:11:49 +0400

    build

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.bitchat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.bitchat"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
