00f3174c6d0303fc089234ec9d58ce6f02559c1a
zack-dev-cm <superkerascv@gmail.com>
2025-07-30 22:47:06 +0400

    fix

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    kotlin("android") version "1.9.22" apply false
+    id("com.android.application") version "8.9.0" apply false
+    kotlin("android") version "2.1.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0" apply false
 }
 
